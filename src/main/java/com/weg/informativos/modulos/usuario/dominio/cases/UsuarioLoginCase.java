e0bfa0bbd02f4a6f74c1ab3595ccdb907172af28
Luca5Eckert <ecklukatv@gmail.com>
2025-07-07 15:42:17 -0300

    feat(usuario): Caso de uso login do usuário.
    Signed-off-by: Luca5Eckert <ecklukatv@gmail.com>

--- src/main/java/com/weg/informativos/modulos/usuario/dominio/cases/UsuarioLoginCase.java
+++ src/main/java/com/weg/informativos/modulos/usuario/dominio/cases/UsuarioLoginCase.java
@@ -1,21 +1,30 @@
 package com.weg.informativos.modulos.usuario.dominio.cases;
 
 import com.weg.informativos.core.dtos.TokenDto;
+import com.weg.informativos.infraestrutura.providers.JwtTokenProvider;
 import com.weg.informativos.modulos.usuario.aplicacao.dtos.UsuarioLoginDto;
 import com.weg.informativos.modulos.usuario.aplicacao.repository.UsuarioRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioLoginCase {
 
-    private final UsuarioRepository usuarioRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationManager authenticationManager;
 
-    public UsuarioLoginCase(UsuarioRepository usuarioRepository) {
-        this.usuarioRepository = usuarioRepository;
+    public UsuarioLoginCase(JwtTokenProvider jwtTokenProvider, AuthenticationManager authenticationManager) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.authenticationManager = authenticationManager;
     }
 
-    public TokenDto executar(UsuarioLoginDto userUsuarioLoginDto){
-        return null;
+    public TokenDto executar(UsuarioLoginDto usuarioLoginDto){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(usuarioLoginDto.email(), usuarioLoginDto.senha()));
+        String token = jwtTokenProvider.generateToken(usuarioLoginDto.email());
+
+        return new TokenDto(token);
     }
 
 }
