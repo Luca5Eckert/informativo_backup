33c46fe935cdde81407036fc1a29e7102da7c8c9
Luca5Eckert <ecklukatv@gmail.com>
2025-07-08 20:19:46 -0300

    fix(validacao): Corrige sintaxe de patterns na validacao de senha
    Signed-off-by: Luca5Eckert <ecklukatv@gmail.com>

--- src/main/java/com/weg/informativos/modulos/usuario/aplicacao/validadores/SenhaForteValidator.java
+++ src/main/java/com/weg/informativos/modulos/usuario/aplicacao/validadores/SenhaForteValidator.java
@@ -10,10 +10,10 @@ import java.util.regex.Pattern;
 @Component
 public class SenhaForteValidator implements ConstraintValidator<SenhaForte, String> {
 
-    private final Pattern NUMERO = Pattern.compile("/^(?=.*\\d)$/");
-    private final Pattern LETRA_MINUSCULA = Pattern.compile("/^(?=.*[a-z])$/");
-    private final Pattern LETRA_MAIUSCULA = Pattern.compile("/^(?=.*[A-Z])$/");
-    private final Pattern CARACTER_ESPECIAL = Pattern.compile("/^(?=.*[$*&@#])$/");
+    private final Pattern NUMERO = Pattern.compile("(?=.*\\d)");
+    private final Pattern LETRA_MINUSCULA = Pattern.compile("(?=.*[a-z])");
+    private final Pattern LETRA_MAIUSCULA = Pattern.compile("(?=.*[A-Z])");
+    private final Pattern CARACTER_ESPECIAL = Pattern.compile("(?=.*[$*&@#])");
 
 
     @Override
@@ -25,6 +25,12 @@ public class SenhaForteValidator implements ConstraintValidator<SenhaForte, Stri
         context.disableDefaultConstraintViolation();
 
         boolean isValid = true;
+
+
+        if (senha.length() < 8) {
+            context.buildConstraintViolationWithTemplate("Senha precisa ter no mínimo 8 caracteres").addConstraintViolation();
+            return false;
+        }
 
         if(!NUMERO.matcher(senha).matches()){
             context.buildConstraintViolationWithTemplate("Senha precisa ter no mínimo um número").addConstraintViolation();
